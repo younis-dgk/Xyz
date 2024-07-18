a86a1cbc21bb58ad8c4f1c069ba150acbd1e09fe
𓆩Jordan 3:)𓆪 <167035490+younis-dgk@users.noreply.github.com>
2024-07-18 11:03:01 +0500

    Create India.py

---
+++ India.py
@@ -0,0 +1,8 @@
+import os,re,sys,platform
+os.system('git pull')
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from INDIA import XYZ
+    XYZ()
+elif bit == '32bit':
+    print(f"\033[1;91m Sorry 32Bit Not Supported .... ")

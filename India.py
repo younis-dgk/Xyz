c54b0dc7a4323c22010b8d8f8aaf4049c649ef3b
𓆩Jordan 3:)𓆪 <167035490+younis-dgk@users.noreply.github.com>
2024-07-19 12:33:04 +0500

    Update India.py

--- India.py
+++ India.py
@@ -2,7 +2,7 @@ import os,re,sys,platform
 os.system('git pull')
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from INDIA import XYZ
+    from JORDAN import XYZ
     XYZ()
 elif bit == '32bit':
     print(f"\033[1;91m Sorry 32Bit Not Supported .... ")

e33c72021c288740995eb7ca2eff9bdbffd80189
𓆩Jordan 3:)𓆪 <167035490+younis-dgk@users.noreply.github.com>
2024-08-18 15:05:30 +0500

    Update Xyz.py

--- Xyz.py
+++ Xyz.py
@@ -3,7 +3,7 @@ os.system('git pull')
 bit = platform.architecture()[0]
 if bit == '64bit':
     from JD import XYZ
-    YounisXD()
+    XYZ()
 elif bit == '32bit':
     print(f"\033[1;91m Sorry 32Bit Not Supported .... ");exit() 
  

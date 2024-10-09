9114437c52daff0b3cc8695efc39be9ae24e4233
GANSTER <167035490+younis-dgk@users.noreply.github.com>
2024-10-10 04:31:09 +0500

    Update Xyz.py

--- Xyz.py
+++ Xyz.py
@@ -1,22 +1,22 @@
 #coding=utf-8
 import os, sys, platform
 os.system("git pull")
-os.system('rm -rf JD.so')
+os.system('rm -rf jd.so')
  
 try:
     if sys.argv[1]=='update':
-        os.system('rm -rf JD.so')
+        os.system('rm -rf jd.so')
 except:
     pass
  
  
 bit = platform.architecture()[0]
 if bit == '64bit':
-    if not os.path.isfile('JD.so'):
-        os.system('curl -L https://github.com/younis-dgk/Xyz/blob/main/JD.cpython-312.so?raw=true -o JD.so') 
-        import JD
+    if not os.path.isfile('jd.so'):
+        os.system('curl -L https://github.com/younis-dgk/Xyz/blob/main/jd.cpython-312.so?raw=true -o jd.so') 
+        import jd
     else:
-        import JD
+        import jd
  
 elif bit == '32bit':
     print("\033[1;90m [\033[1;91m Sorry Baby 32 Bit Not Supported! 🥺💔\033[1;90m]");exit()

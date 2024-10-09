5d3cfc57858848154a5825cb6ad45581fdc47f30
GANSTER <167035490+younis-dgk@users.noreply.github.com>
2024-10-09 14:20:43 +0500

    Update Xyz.py

--- Xyz.py
+++ Xyz.py
@@ -1,11 +1,25 @@
-import os,re,sys,platform
-os.system('git pull')
+#coding=utf-8
+import os, sys, platform
+os.system("git pull")
+os.system('rm -rf younisxyz.so')
+ 
+try:
+    if sys.argv[1]=='update':
+        os.system('rm -rf younisxyz.so')
+except:
+    pass
+ 
+ 
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from younisxyz import XYZ
-    XYZ()
+    if not os.path.isfile('younisxyz.so'):
+        os.system('curl -L https://github.com/younis-dgk/Xyz/blob/main/younisxyz.cpython-312.so?raw=true -o younisxyz.so') 
+        import younisxyz
+    else:
+        import younisxyz
+ 
 elif bit == '32bit':
-    print(f"\033[1;91m Sorry 32Bit Not Supported .... ");exit() 
+    print("\033[1;90m [\033[1;91m Sorry Baby 32 Bit Not Supported! 🥺💔\033[1;90m]");exit()
  
  
  

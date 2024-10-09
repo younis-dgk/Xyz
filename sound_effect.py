47916c2465907c40a18d14b761efd14fe2ffb754
GANSTER <167035490+younis-dgk@users.noreply.github.com>
2024-10-10 04:35:19 +0500

    Add files via upload

---
+++ sound_effect.py
@@ -0,0 +1,17 @@
+"""
+    @ script by ---( Younis john )---
+    @ Github : https://github.com/younis-dgk
+    @ WhatsApp : +923194999455
+    
+"""
+import random 
+import os 
+num=random.randint(1,2)
+print(num)
+os.system("clear")
+print("\033[1m\033[33m[\033[32m---Booting--\033[33m]\033[0m")
+
+if num == 1:
+    os.system("mpv Access-Granted.mp3")
+else :
+    os.system("mpv JARVIS2.mp3")

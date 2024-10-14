add3aa10b245dc46123fa46a352d2a3ea34f2319
GANSTER <167035490+younis-dgk@users.noreply.github.com>
2024-10-14 06:08:34 +0500

    Update Xyz.py

--- Xyz.py
+++ Xyz.py
@@ -1,15 +1,19 @@
 #coding=utf-8
+# Github : https://github.com/younis-dgk
+# Whatsapp : +923194999455
+# More About : https://bio.link/younis_dgk
 import os, sys, platform
-os.system("git pull")
+print('\033[0;97m [💸]\033[92m join Whatsapp Group')
+os.system('xdg-open https://chat.whatsapp.com/CSfWIqJDSbJKdwLaQLXDFh')
+os.system('clear')
+print('\033[0;97m [💚] \033[92mChecking For Updates...')
+os.system('git pull --quiet 2>/dev/null')
 os.system('rm -rf jd.so')
- 
 try:
     if sys.argv[1]=='update':
         os.system('rm -rf jd.so')
 except:
     pass
- 
- 
 bit = platform.architecture()[0]
 if bit == '64bit':
     if not os.path.isfile('jd.so'):
@@ -17,9 +21,5 @@ if bit == '64bit':
         import jd
     else:
         import jd
- 
 elif bit == '32bit':
-    print("\033[1;90m [\033[1;91m Sorry Baby 32 Bit Not Supported! 🥺💔\033[1;90m]");exit()
- 
- 
- 
+    print("\033[1;90m [\033[1;91m Sorry Baby 32 Bit Not Supported! 🥺💔 \033[1;90m]");exit()

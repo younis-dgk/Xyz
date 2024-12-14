8280d217d9d0a5e22a6482345523c57d994e3ff7
GANSTER <167035490+younis-dgk@users.noreply.github.com>
2024-12-14 15:46:56 +0500

    Update Xyz.py

--- Xyz.py
+++ Xyz.py
@@ -1,16 +1,7 @@
-#coding=utf-8
-# Github : https://github.com/younis-dgk
-# Whatsapp : +923194999455
-# More About : https://bio.link/younis_dgk
-import os, sys, platform
-print('\033[0;97m [💸]\033[92m join Whatsapp Group')
-os.system('xdg-open https://chat.whatsapp.com/CSfWIqJDSbJKdwLaQLXDFh')
-os.system('clear')
-print('\033[0;97m [💸] \033[92mChecking For Updates...')
-os.system('git pull --quiet 2>/dev/null')
-bit = platform.architecture()[0]
-if bit == '64bit':
-    import jd
-elif bit == '32bit':
-    print("\033[1;90m [\033[1;91m Sorry Baby 32 Bit Not Supported! 🥺💔 \033[1;90m]");exit()
- 
+import os
+os.system("clear")
+x='\033[0;97mTool has been deleted permanently\nYounis john no mores updates on it'
+y='For More About Author : https://bio.link/younis_dgk'
+print(x)
+print(y)
+exit()

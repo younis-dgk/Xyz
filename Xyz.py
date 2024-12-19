e707222114d7be78555a8f70a9caf58b70ae6118
GANGSTER <167035490+younis-dgk@users.noreply.github.com>
2024-12-19 10:26:59 +0500

    Update Xyz.py

--- Xyz.py
+++ Xyz.py
@@ -5,28 +5,16 @@
     @ facebook : https://www.facebook.com/YounisDgk
     
 """
-import os, sys, platform
+import os, sys,time, platform
 os.system('clear') 
 print('\033[0m [💸] \033[92m join Our WhatsApp group For More Updates 🥰✨') 
+time.sleep(3)
 os.system('xdg-open https://chat.whatsapp.com/CSfWIqJDSbJKdwLaQLXDFh')
 print('\n\033[0m [\033[92m✓\033[97m] \033[92m Checking For Updates ....\n') 
-os.system('rm -rf Xyz.so')
- 
-try:
-    if sys.argv[1]=='update':
-        os.system('rm -rf Xyz.so')
-except:
-    pass
- 
- 
+
 bit = platform.architecture()[0]
 if bit == '64bit':
-    if not os.path.isfile('Xyz.so'):
-        os.system('curl -L https://github.com/younis-dgk/Xyz/blob/main/Xyz.cpython-312.so?raw=true -o Xyz.so') 
-        import Xyz
-    else:
-        import Xyz
- 
+    import Xyz
 elif bit == '32bit':
     print("\033[0m\033[91m Sorry Your Device Is 32bit, This Tool 32bit Not Sported, Only For \033[92m64Bit\033[0m ]");exit() 
  
